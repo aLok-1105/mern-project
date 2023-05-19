b1d32dc845875477432a247e07cca83c2b7e6c63
aLok-1105 <alokranjan11052003@gmail.com>
2023-05-19 17:19:29 +0530

    securing-code-mongoose

---
+++ server/app.js
@@ -0,0 +1,50 @@
+/** @format */
+
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+
+dotenv.config({ path: './config.env' });
+require('./db/conn');
+
+const PORT = process.env.PORT;
+
+
+
+//Middleware
+const mildleware = (req, res, next) => {
+	console.log('Milddleware');
+	next();
+};
+
+app.get('/', (req, res) => {
+	res.send('Hello World');
+});
+
+app.get('/about', mildleware, (req, res) => {
+	res.send('About');
+});
+
+app.get('/contact', (req, res) => {
+	res.send('Contact');
+});
+
+app.get('/signin', (req, res) => {
+	res.send('Contact');
+});
+
+app.get('/signup', (req, res) => {
+	res.send('Contact');
+});
+
+app.listen(PORT, () => {
+	console.log(`Server is running at ${PORT}`);
+});
+
+// , {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false
+// }
