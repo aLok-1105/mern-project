7df844f5aa7e209210cb7e04f5999d138c51fa75
aLok-1105 <alokranjan11052003@gmail.com>
2023-05-20 11:19:39 +0530

    storing-userdata-mongo-async-await

--- server/router/auth.js
+++ server/router/auth.js
@@ -8,7 +8,7 @@ router.get('/', (req, res) => {
 	res.send('Hello World from auth');
 });
 
-router.post('/register', (req, res)=>{
+router.post('/register', async (req, res)=>{
 
     const {name, email, work, phone, password, cpassword} = req.body;
 
@@ -16,19 +16,49 @@ router.post('/register', (req, res)=>{
         return res.status(422).json({error:"Invalid"})
     }
 
-    User.findOne({email:email}).then((UserExists)=>{
-        if(UserExists){
-            return res.status(422).json({error:"Email exists!!"})
-        }
+    try {
+        const userExists = await User.findOne({email:email});
+            if(userExists){
+                return res.status(422).json({error:"Email exists!!"})
+            }    
 
-        const user = new User({name, email, work, phone, password, cpassword});
+            const user = new User({name, email, work, phone, password, cpassword});
+    
+            await user.save();
 
-        user.save().then(()=>{
             res.status(201).json({message: "User Successfully Registered"});
-        }).catch((err)=>res.status(500).json({error: "Failed to register"}));
-    }).catch((err)=>{console.log(err)})
 
-
+        
+    } catch (error) {
+        console.log(error);
+    }
 })
+
+
+
+//promises
+
+// router.post('/register', (req, res)=>{
+
+//     const {name, email, work, phone, password, cpassword} = req.body;
+
+//     if(!name || !email || !work || !phone || !password || !cpassword){
+//         return res.status(422).json({error:"Invalid"})
+//     }
+
+//     User.findOne({email:email}).then((UserExists)=>{
+//         if(UserExists){
+//             return res.status(422).json({error:"Email exists!!"})
+//         }
+
+//         const user = new User({name, email, work, phone, password, cpassword});
+
+//         user.save().then(()=>{
+//             res.status(201).json({message: "User Successfully Registered"});
+//         }).catch((err)=>res.status(500).json({error: "Failed to register"}));
+//     }).catch((err)=>{console.log(err)})
+
+
+// })
 
 module.exports = router;
