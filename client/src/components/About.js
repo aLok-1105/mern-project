2939b8d76907865d06b18de0a7b9352325a7999f
aLok-1105 <alokranjan11052003@gmail.com>
2023-05-21 14:43:59 +0530

    storing-input-data

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -2,6 +2,65 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <>
+      <div className='container'>
+        <form method=''>
+          <div className='row'>
+            <div className='col-md-4'>
+              <img src=' ' alt='img' />
+            </div>
+            <div className='col-md-6'>
+              <div className='profile'>
+                <h5>Name</h5>
+                <p>rank</p>
+                <ul class="nav nav-tabs">
+                      <li class="active"><a href="#home">Home</a></li>
+                      <li><a href="#timeline">Menu 1</a></li>
+                    </ul>
+              </div>
+            </div>
+            <div className='col-md-2'>
+              <div className='edit-profile'>
+                <button type='button' >Edit profile</button>
+              </div>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='col-md-4'>
+              <div>
+                <p>Work Links</p>
+                <a href='#'>Insta</a>
+              </div>
+            </div>
+            <div className='col-md-8 pl-5'>
+              <div className='tab-content' id='tab-content'>
+                <div id='home'>
+                  <div className='row'>
+                    <div className='col-md-6'>
+                      <label>Name</label>
+                    </div>
+                    <div className='col-md-6'>
+                      <label>Name</label>
+                    </div>
+
+                  </div>
+                </div>
+                <div id='#timeline'>
+                  <div className='row'>
+                    <div className='col-md-6'>
+                      <label>Timeline</label>
+                    </div>
+                    <div className='col-md-6'>
+                      <label>Name</label>
+                    </div>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
   )
 }
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Signup() {
+
+  const [user, setUser] = useState({
+    name: '',
+    email:'',
+    phone: '',
+    work: '',
+    password: '',
+    cpassword: ''
+  })
+
+  var name, value;
+
+  const handleInputs = (event)=>{
+    console.log(event);
+    name = event.target.name;
+    value = event.target.value;
+    setUser({...user, [name]:value});
+  }
+
   return (
     <>
     <section className='signup'>
@@ -9,31 +28,31 @@ export default function Signup() {
           <div className='signup-form' >
             <h2 className='form-title'>Create your account</h2>
             <form >
-              <div class="mb-3">
-                <label for="name" class="form-label"><i class="zmdi zmdi-account zmdi-hc-1x me-2"></i>Name</label>
-                <input type="name" class="form-control" id="name" aria-describedby="emailHelp" />
+              <div className="mb-3">
+                <label for="name" className="form-label"><i className="zmdi zmdi-account zmdi-hc-1x me-2"></i>Name</label>
+                <input type="name" className="form-control" name='name' id="name" value={user.name} onChange={handleInputs} aria-describedby="emailHelp" />
               </div>
-              <div class="mb-3">
-                <label for="email" class="form-label"><i class="zmdi zmdi-account zmdi-hc-1x me-2" />Email address</label>
-                <input type="email" class="form-control" id="email" aria-describedby="emailHelp" />
+              <div className="mb-3">
+                <label for="email" className="form-label"><i className="zmdi zmdi-account zmdi-hc-1x me-2" />Email address</label>
+                <input type="email" className="form-control" name='email' id="email" value={user.email} onChange={handleInputs} aria-describedby="emailHelp" />
               </div>
-              <div class="mb-3">
-                <label for="phone" class="form-label"><i class="zmdi zmdi-account zmdi-hc-1x me-2" />Phone</label>
-                <input type="phone" class="form-control" id="phone" aria-describedby="emailHelp" />
+              <div className="mb-3">
+                <label for="phone" className="form-label"><i className="zmdi zmdi-account zmdi-hc-1x me-2" />Phone</label>
+                <input type="phone" className="form-control" name='phone' value={user.phone} onChange={handleInputs} id="phone" aria-describedby="emailHelp" />
               </div>
-              <div class="mb-3">
-                <label for="work" class="form-label"><i class="zmdi zmdi-account zmdi-hc-1x me-2" />Profession</label>
-                <input type="work" class="form-control" id="work" aria-describedby="emailHelp" />
+              <div className="mb-3">
+                <label for="work" className="form-label"><i className="zmdi zmdi-account zmdi-hc-1x me-2" />Profession</label>
+                <input type="work" className="form-control" name='work' value={user.work} onChange={handleInputs} id="work" aria-describedby="emailHelp" />
               </div>
-              <div class="mb-3">
-                <label for="password" class="form-label"><i class="zmdi zmdi-account zmdi-hc-1x me-2" />Password</label>
-                <input type="password" class="form-control" id="password" aria-describedby="emailHelp" />
+              <div className="mb-3">
+                <label for="password" className="form-label"><i className="zmdi zmdi-account zmdi-hc-1x me-2" />Password</label>
+                <input type="password" className="form-control" name='password' value={user.password} onChange={handleInputs} id="password" aria-describedby="emailHelp" />
               </div>
-              <div class="mb-3">
-                <label for="cpassword" class="form-label"><i class="zmdi zmdi-account zmdi-hc-1x me-2" />Confirm Password</label>
-                <input type="password" class="form-control" id="cpassword" aria-describedby="emailHelp" />
+              <div className="mb-3">
+                <label for="cpassword" className="form-label"><i className="zmdi zmdi-account zmdi-hc-1x me-2" />Confirm Password</label>
+                <input type="password" className="form-control" name='cpassword' value={user.cpassword} onChange={handleInputs} id="cpassword" aria-describedby="emailHelp" />
               </div>
-              <button type="submit" name="signup" id='signup' class="btn btn-primary">Submit</button>
+              <button type="submit" name="signup" id='signup' className="btn btn-primary">Submit</button>
       </form>
 
           </div>
